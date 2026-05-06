2121e32cd7acf36712603dc8c54b691e80fe4c78
adityamsaxena <79154443+adityamsaxena@users.noreply.github.com>
2026-05-06 11:13:11 +0530

    Add alternate solution for filtering fruits with two vowels
    Added an alternate solution using a different approach to count vowels.

--- List_&_List_Comprehension/LCEx4_Exactly_Two_Vowels.py
+++ List_&_List_Comprehension/LCEx4_Exactly_Two_Vowels.py
@@ -7,3 +7,9 @@ fruits_with_only_two_vowels = [fruit for fruit in fruits if (
     fruit.count("u")) == 2]
 
 print(fruits_with_only_two_vowels)
+
+#Alternate Solution:-->
+# fruits = ['mango', 'kiwi', 'strawberry', 'guava', 'pineapple', 'mandarin orange']
+# vowels = "aeiou"
+# fruits_with_only_two_vowels = new_fruit = [item for item in fruits if sum(ch in vowels for ch in item) == 2]
+# print(fruits_with_only_two_vowels)
